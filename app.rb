8a2a31c1b98892446dd65cd77ae191598791e566
itggot-emil-lindblad <emil.lindblad@elev.ga.ntig.se>
2019-06-28 00:44:48 +0200

    Added all routes

--- app.rb
+++ app.rb
@@ -1,5 +1,33 @@
 require 'sinatra'
 
+configure do 
+    set :bind, '0.0.0.0'
+end
+
 get('/') do 
     slim(:index)
+end
+
+get('/nyheter') do
+    slim(:nyheter)
+end
+
+get('/verksamhet') do
+    slim(:verksamhet)
+end
+
+get('/personal') do
+    slim(:personal)
+end
+
+get('/blanketter') do
+    slim(:blanketter)
+end
+
+get('/lankar') do
+    slim(:lankar)
+end
+
+get('/kontaktinfo') do
+    slim(:kontaktinfo)
 end
