a5e359699fcb613b66c4021297db9bed857c28fb
elindblad <emil.lindblad0901@gmail.com>
2019-08-19 19:40:57 +0200

    Added login for mobile

--- app.rb
+++ app.rb
@@ -1,8 +1,37 @@
 require 'sinatra'
+require_relative 'model.rb'
+enable :sessions
 
 configure do 
     set :bind, '0.0.0.0'
 end
+
+helpers do
+    def set_error(msg)
+        session[:error] = msg
+    end
+    
+    def get_error()        
+        error = session[:error]
+        session[:error] = nil
+        return error
+    end
+
+    def error?
+        !session[:error].nil?
+    end
+
+    def login_status(usr)
+        session[:username] = usr
+    end
+
+    def get_login_status()
+        usr = session[:username]
+        return usr
+    end
+end
+
+include Model
 
 get('/') do 
     slim(:index)
@@ -30,4 +59,24 @@ end
 
 get('/kontaktinfo') do
     slim(:kontaktinfo)
+end
+
+get('/login') do 
+    slim(:login)
+end
+
+post('/login') do
+    login = Users.login(params)
+    if login != false
+        login_status(login)
+        redirect('/')
+    else
+        set_error("Fel användarnamn eller lösenord!")
+        redirect('/login')
+    end
+end
+
+post('/logout') do
+    session.destroy
+    redirect(back)
 end
---
+++ model.rb
@@ -0,0 +1,30 @@
+module Model
+    # Loads the databse
+    def self.connect
+        Sequel.connect(ENV['DATABASE_URL'] || 'sqlite://db/data.db')
+    end
+
+    module Users
+        # Hashes inputpassword and compares it to the hash in database
+        def self.checkpassword(pw,dbpw)
+            if BCrypt::Password.new(dbpw) == pw
+                return true
+            else
+                return false
+            end
+        end
+
+        # Attempts to authenticate the user
+        def self.login(params)
+            db = Model::connect()
+            result = db[:users].first(:UserName => params["UserName"])
+            if result == nil
+                return false
+            elsif checkpassword(params["PassWord"],result[:Hash]) == true
+                return result[:UserName]
+            else
+                return false
+            end
+        end
+    end
+end
