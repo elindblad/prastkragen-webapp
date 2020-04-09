2a8e846a017f21962cd8a8679d89ca36bd9a6375
elindblad <emil@lindblad.tech>
2020-04-09 23:55:25 +0200

    Code cleanup

--- public/js/navscroll.js
+++ public/js/navscroll.js
@@ -1,15 +1,9 @@
 $(function() {
-    console.log($(window).scrollTop())
     $(window).scroll(function() {
         if ($(window).scrollTop() >= 566) {
             $("header.desktop").addClass("dark");
         } else {
             $("header.desktop").removeClass("dark");
         }
-        // if ($(window).scrollTop() >= 165) {
-        //     $("nav.headermobile").addClass("fixed");
-        // } else {
-        //     $("nav.headermobile").removeClass("fixed");
-        // }
     });
 });
